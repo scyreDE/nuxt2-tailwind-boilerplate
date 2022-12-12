3af1f3c3dd39d97dd05e0869782fa48cc6ef0add
Simon Kramer <simon@sellerpoint.com>
2022-12-12 17:47:48 +0100

    Inital commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="grid h-screen place-items-center">
+    <h1>Welcome to your Nuxt 2 Boilerplate powered with TailwindCSS</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
